179d39c2bb5fa5f3671fccdeb4d69b0c3aa9993f
Yash Wankar <yashwankar25@gmail.com>
2022-05-07 05:41:03 +0530

    script for exporting the document

--- scripts/forExport.js
+++ scripts/forExport.js
@@ -2,6 +2,7 @@ function generatePDF(){
     const element = document.getElementById("certificate");
     let opt = {
         filename: "certificate.pdf",
+        image: {quality:1},
         jsPDF: {orientation: 'landscape'}
     };
     html2pdf().set(opt).from(element).toPdf().save();
