2356b612178f5ae3bd5a26bc1222e7d381c279a6
Yash Wankar <yashwankar25@gmail.com>
2022-05-08 20:24:46 +0530

    working phase 3.4 - added preview components

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -36,30 +36,35 @@ function addFontSelectOptions(cls){
         'Vampiro One', 
         'Yellowtail'
     ];
-
-    let items = document.querySelectorAll(`${cls} .perso-items select`);
-    for(let i=0; i<items.length; i++){
+    let items = '';
+    if(cls.startsWith('.')){
+        items = document.querySelectorAll(`${cls} .perso-items select`);
+        for(let i=0; i<items.length; i++){
+            for(let j=0; j<fontNames.length; j++){
+                let option = document.createElement("option");
+                option.value = fontNames[j];
+                option.text = fontNames[j];
+                option.style.fontFamily = fontNames[j];
+                items[i].appendChild(option);
+            }
+        }
+    }else{
+        items = document.getElementById(cls);
         for(let j=0; j<fontNames.length; j++){
             let option = document.createElement("option");
             option.value = fontNames[j];
             option.text = fontNames[j];
             option.style.fontFamily = fontNames[j];
-            items[i].appendChild(option);
+            items.appendChild(option);
         }
     }
+    
 }
 
 
 function persoStyle(a, b){
     let source = document.getElementById(a);
-    let dest = "";
-    if(b == 'p'){
-        dest = document.createElement('p');
-    }else if(b == 'img'){
-        dest = document.createElement('img');
-    }else{
-        dest = document.getElementById(b);
-    }
+    let dest = document.getElementById(b);
     dest.style.position = 'absolute';
     dest.style.fontFamily = source.children[2].value;
     dest.style.color = source.children[4].value.toString();
@@ -84,7 +89,13 @@ function expandProcedureItems(index){
 function certInit(index){
     
 }
-
+function addTextListeners(){
+    document.addEventListener("change", function(){
+        for(let textNo=1; textNo < textCount; textNo++)
+            persoStyle(`addText${textNo}`,`addTextToFrame${textNo}`);
+        console.log("added listener successfully");
+    });
+}
 
 let textCount = 1;
 function additionalText(){
@@ -92,28 +103,40 @@ function additionalText(){
     let parent = document.querySelector('.additionalText');
 
     let element = document.createElement('div');
+
+    let title = document.createElement('p');
+    title.innerHTML = `Text-${textCount} :- `;
+    title.className = 'perso-title';
+    title.style.padding = '10px 20px 0 0';
+
     let inp = document.createElement('input');
     inp.type = 'text';
     inp.className = 'additional-text';
     inp.placeholder = 'Type text here...';
 
+    element.appendChild(title);
     element.appendChild(inp);
+    element.style.display = 'flex';
+    element.style.padding = '5pxpx';
+    element.style.justifyContent = 'center';
     parent.appendChild(element);
 
     let choices = document.createElement('div');
     choices.className = 'perso-items';
     choices.id = `addText${textCount}`;
 
-    let title = document.createElement('p');
-    title.innerHTML = `Text-${textCount} :- `;
-    title.className = 'perso-title';
-    choices.appendChild(title);
-
     let x = document.createElement('p');
+    x.innerHTML = `Text-${textCount} :- `;
+    x.className = "perso-title";
+    choices.appendChild(x);
+
+    x = document.createElement('p');
     x.innerHTML = "Font:";
     choices.appendChild(x);
 
-    choices.appendChild(document.createElement('select'));
+    x = document.createElement('select');
+    x.id = `addTextSelect${textCount}`;
+    choices.appendChild(x);
 
     x = document.createElement('p');
     x.innerHTML = "Color:";
@@ -150,13 +173,42 @@ function additionalText(){
     choices.appendChild(x);
 
     parent.appendChild(choices);
-    textCount++;
-    addFontSelectOptions(".preview");
-}
-function additionalImage(){
+    addFontSelectOptions(`addTextSelect${textCount}`);
+    
+    let newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.value = `Type here... (${textCount})`;
+    newInput.id = `addTextToFrame${textCount}`;
 
+    document.getElementById("certificate").appendChild(newInput); 
+    textCount++;
+}
+
+let imgCount = 1;
+function additionalImage(){
+    let parent = document.querySelector('.additionalImage');
+
+    let element = document.createElement('div');
+    element.className = 'addional-image-element';
+
+    let title = document.createElement('p');
+    title.innerHTML = `Image-${imgCount} :- `;
+    title.className = 'perso-title';
+    title.style.padding = '10px 20px 0 0';
+
+    let inp = document.createElement('input');
+    inp.type = 'file';
+    inp.className = 'additional-image';
+    inp.placeholder = 'Type text here...';
+
+
+    element.appendChild(title);
+    element.appendChild(inp);
+    
+    parent.appendChild(element);
 }
 
 addFontSelectOptions(".personalize");
+addTextListeners();
 document.getElementById("addText").addEventListener("click", additionalText);
 document.getElementById("addImage").addEventListener("click", additionalImage);
